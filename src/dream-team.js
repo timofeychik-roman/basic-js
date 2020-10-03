62fd4d4918902cb6ec24f4d325446c29a3668522
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-03 16:18:04 +0300

    feat: add dream-team function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  debugger;
+  if (members == undefined || members.length == 0)
+    return false;
+  let names = [];
+  for (let i = 0; i < members.length; i++)
+    if (typeof(members[i]) == "string") {
+      let j = 0;
+      while (members[i][j] == " ")
+        j++;
+      names.push(members[i][j]);
+    }
+  if (names.length == 0)
+    return false;
+  for (let i = 0; i < names.length; i++)
+    names[i] = names[i].toUpperCase();
+  names = names.sort();
+  names = names.join("");  
+  return names;
 };
