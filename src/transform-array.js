f52bd8bbb59f6f85b4c6e67ac0ab4099c9f7988d
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-05 02:46:47 +0300

    feat: add transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let a = arr.slice(0, arr.length);
+  for (let i = 0; i < a.length; i++){
+    switch(a[i]) {
+      case "--discard-next":
+        if (i == a.length - 1)
+          a.splice(i, 1);
+        else {
+          a[i + 1] = "*";
+          a.splice(i, 1);
+        }
+        break;
+      case "--discard-prev":
+        if (i == 0)
+          a.splice(i, 1)
+        else {
+          a[i - 1] = "*";
+          a.splice(i, 1)
+        }
+        break;
+      case "--double-next":
+        if (i == a.length - 1)
+          a.splice(i, 1);
+        else if (a[i + 1] == "*")
+          a.splice(i, 1);
+        else 
+          a[i] = a[i + 1];
+        break;
+      case "--double-prev":
+        if (i == 0)
+          a.splice(i, 1);
+        else if (a[i - 1] == "*")
+          a.splice(i, 1);
+        else 
+          a[i] = a[i - 1];
+        break;
+    }
+  }
+  for (let i = 0; i < a.length; i++)
+    if (a[i] == "*"){
+      a.splice(i, 1);
+      i--;
+    }
+ /* if (a[0] == "*"){
+    let b = a.slice(1, a.length);
+    return b;
+  }*/
+  return a;
 };
