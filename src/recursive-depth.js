55a804855bb442358df09e0212dd7cb2b31e7e04
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-05 20:29:29 +0300

    feat: fix recursive-depth function

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,14 +2,9 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
-    let flag = false;
     for (let i = 0; i < arr.length; i++)
-      if (Array.isArray(arr[i])) {
-        flag = true;
-        break;
-      }
-    if (flag)
-      return 1 + this.calculateDepth(arr.flat());
-    else return 1;
+      if (Array.isArray(arr[i]))
+        return 1 + this.calculateDepth(arr.flat());
+    return 1;
   }
 };
