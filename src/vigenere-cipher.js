a6d3f9812286491b8a52b5241aaed284ab2a480a
timofeychik-roman <timofeychik.roman@yandex.ru>
2020-10-06 21:49:50 +0300

    feat: add vigenere-cipher function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,51 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor (isdirect) {
+    if (isdirect == undefined)
+      this.direct = true;
+    else this.direct = isdirect;
+  }
+  encrypt(str1, pass1) {
+    let str = str1.toUpperCase();
+    let pass = pass1.toUpperCase();
+    let isLetter = /^[A-Z]+$/;
+    let res = "";
+    if (typeof(str) != "string" || typeof(pass) != "string")
+      throw new Error("THROWN");
+    let countNotLetter = 0;
+    for (let j = 0; j < str.length; j++) {
+      if (isLetter.test(str[j]))
+        res += String.fromCharCode((str.charCodeAt(j) - "A".charCodeAt() + pass.charCodeAt((j - countNotLetter)%pass.length) - "A".charCodeAt()) % 26 + "A".charCodeAt());
+      else {
+        res += str[j];
+        countNotLetter++;
+      }
+    }
+    if (this.direct)
+      return res;
+    else return res.split("").reverse().join("");
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(res, pass1) {
+    let pass = pass1.toUpperCase();
+
+    let isLetter = /^[A-Z]+$/;
+    let str = "";
+    if (typeof(res) != "string" || typeof(pass) != "string")
+      throw new Error("THROWN");
+    let i = 0;
+    let countNotLetter = 0;
+    for (let j = 0; j < res.length; j++) {
+      if (isLetter.test(res[j]))
+        str += String.fromCharCode((res.charCodeAt(j) - "A".charCodeAt() + 26 - pass.charCodeAt((j - countNotLetter)%pass.length) + "A".charCodeAt()) % 26 + "A".charCodeAt());
+      else {
+        str += res[j];
+        countNotLetter++;
+      }
+    }
+    if (this.direct)
+      return str;
+    else return str.split("").reverse().join("");
   }
 }
 
